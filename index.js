96a5f547ff4d1a828b36fae4458a5a9329b4f533
nandni94 <nandnibairwa94@gmail.com>
2024-06-08 11:49:20 +0530

    basic

---
+++ index.js
@@ -0,0 +1,53 @@
+// //var 
+// var num=10
+// {
+//     num=20;
+// }
+
+// console.log("var",num)
+// //let
+// let num1=10
+// {
+//     num1=20;
+// }
+// console.log(num1)
+// //const
+// const num2={"name":"raj"}
+// {
+// num2.name="hello"
+// }
+// console.log(num2)
+// var num=10
+// {
+//     var num=20
+//     num=30
+//     console.log(num)
+// }
+// console.log(num)
+
+// const num1=10
+// {
+//     const num2=10
+//     console.log(num1)
+// }
+// console.log(num2);
+// let num1=10;
+// let num2=20;
+// let sum=num1+num2;
+// console.log(sum)
+// let multiply=num1*num2;
+// console.log(multiply)
+// let divide=num1/num2;
+// console.log(divide)
+// let num1=5;
+// let num2=10;
+// let sum= --num1+ ++num2 + num1-- + num2++
+// console.log(sum)
+// console.log(num1)
+// console.log(num2)
+// let checkIn=true
+// let checkOut=true;
+// console.log(checkOut && checkOut);
+// let age=18
+// let result=(age>=18)?"yes":"no"
+// console.log(result);
